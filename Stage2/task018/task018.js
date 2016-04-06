1143a8f18a9785e0bf013a44915bcca30d07a918
jaywii <jaywiiwho@gmail.com>
2016-04-06 22:17:02 +0800

    upload task018

---
+++ Stage2/task018/task018.js
@@ -0,0 +1,69 @@
+window.onload = function() {
+  $ = function(ele) {
+    return document.querySelector(ele);
+  };
+  var input = $('input');
+  var display = $('.display');
+  var data = [];
+
+  function initData(mood) {
+    var inputValue = input.value;
+    var test = /^\d+$/.test(inputValue);
+    if (test) {
+      switch (mood) {
+        case 'push':
+          data.push(inputValue);
+          break;
+        case 'pop':
+          data.pop(inputValue);
+          break;
+        case 'shift':
+          data.shift(inputValue);
+          break;
+        case 'unshift':
+          data.unshift(inputValue);
+          break;
+      }
+    }else{
+      alert("请输入数字");
+      input.value = "";
+    }
+
+  }
+
+  function addEle() {
+    display.innerHTML = data.map(function(d) {
+      return "<div>" + d + "</div>";
+    }).join('');
+  }
+
+  function output(mood) {
+    console.log(mood);
+    initData(mood);
+    addEle();
+  }
+
+  function remove(e) {
+    var target = e.target || window.target;
+    //获取点击元素的下标
+    var index = [].indexOf.call(target.parentNode.children, target);
+    data.splice(index, 1);
+    addEle();
+  }
+
+  $("#left-in").onclick = function() {
+    output("unshift");
+  };
+  $("#right-in").onclick = function() {
+    output("push");
+  };
+  $("#left-out").onclick = function() {
+    output("shift");
+  };
+  $("#right-out").onclick = function() {
+    output("pop");
+  };
+  display.onclick = function(e) {
+    remove(e);
+  };
+};
